44ed19004e1c608abf8f69424c140c8a0ee229f6
Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
2020-05-22 17:50:51 +0200

    Added BFS

---
+++ BFS.py
@@ -0,0 +1,47 @@
+import time
+
+from heapdict import heapdict
+
+from Cities import C
+
+
+class BFS:
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def distanceToNextNode(self, current, neighbor):
+        if current == neighbor:
+            return 0
+        return self.matrix[current][neighbor]
+
+    def reconstruct_path(self, came_from, current):
+        total_path = [current]
+        while current in came_from.keys():
+            current = came_from[current]
+            total_path.insert(0, current)
+        return total_path
+
+    def resolve(self, start, goal):
+        startTime = time.time()
+        open_set = heapdict()
+        open_set[start] = 0
+        visited = {}
+        for city in C:
+            visited[city] = 0
+        visited[start] = 1
+        came_from = {}
+
+        while len(open_set) != 0:
+            current = open_set.popitem()  # Obtenemos una city y su f_score
+            current_postition = current[0].value
+            current_name = current[0].name
+            if current_postition == goal.value:
+                endTime = time.time()
+                return current[1], self.reconstruct_path(came_from, current_name), endTime - startTime
+
+            for neighbor, column in enumerate(self.matrix[current_postition]):
+                city = C(neighbor)
+                if visited[city] != 1 and column < 99999:
+                    visited[city] = 1
+                    came_from[city.name] = current_name
+                    open_set[city] = self.distanceToNextNode(neighbor, goal.value)
--- data.py
+++ data.py
@@ -4,16 +4,21 @@ import time
 from AStar import AStar
 from Cities import C
 from Dijkstra import Dijkstra
+from BFS import BFS
 
 if __name__ == '__main__':
     matrix = numpy.loadtxt(open("data.csv", "rb"), delimiter=",", skiprows=1, dtype=int)
 
-    rightTimes = 0
+    rightTimesStar = 0
+    rightTimesBFS = 0
     totalCases = 0
     totalTimeStar = 0
+    totalTimeBFS = 0
     totalTimeDist = 0
     astar = AStar(matrix)
     disktra = Dijkstra(matrix)
+    bfs = BFS(matrix)
+
     for city in C:
         for city2 in C:
             print("From :" + city.name + "\t" "to :" + city2.name)
@@ -23,20 +28,32 @@ if __name__ == '__main__':
             (costDisk, pathDisk, timeDisk) = disktra.resolve(city, city2)
             print(
                 "Disk GOT: " + "Cost of:" + str(costDisk) + " Path of: " + str(pathDisk) + " Time of: " + str(timeDisk))
+            (costBfs, pathBfs, timeBfs) = bfs.resolve(city, city2)
+            print(
+                "Bfs GOT: " + "Cost of:" + str(costBfs) + " Path of: " + str(pathBfs) + " Time of: " + str(timeBfs))
 
+            totalTimeBFS += timeBfs
             totalTimeDist += timeDisk
             totalTimeStar += timeAStar
             if pathAStar == pathDisk:
-                rightTimes += 1
+                rightTimesStar += 1
+            if pathBfs == pathDisk:
+                rightTimesBFS += 1
             totalCases += 1
 
     print("Total Cases " + str(totalCases))
-    print("Right Cases " + str(rightTimes))
-    print("Right Percentaje " + str((rightTimes / totalCases) * 100)+"%")
+    print("Right Cases Star " + str(rightTimesStar))
+    print("Right Percentaje " + str((rightTimesStar / totalCases) * 100) + "%")
 
+    print("Right Cases BFS " + str(rightTimesBFS))
+    print("Right Percentaje " + str((rightTimesBFS / totalCases) * 100) + "%")
+
+    mediaTiempoBfs = totalTimeBFS / totalCases
     mediaTiempoStar = totalTimeStar / totalCases
     mediaTiempoDist = totalTimeDist / totalCases
-    print("Media tiempo Star " + str(mediaTiempoStar))
-    print("Media tiempo Disk " + str(mediaTiempoDist))
-    print("Porcentaje de mejora " + str((mediaTiempoDist/mediaTiempoStar)*100)+"%")
 
+    print("Media tiempo BFS " + str(mediaTiempoBfs * 1000) + " ms")
+    print("Media tiempo Star " + str(mediaTiempoStar * 1000) + " ms")
+    print("Media tiempo Disk " + str(mediaTiempoDist * 1000) + " ms")
+    print("Porcentaje de mejora Astar " + str((mediaTiempoDist / mediaTiempoStar) * 100) + "%")
+    print("Porcentaje de mejora BFS " + str((mediaTiempoBfs / mediaTiempoStar) * 100) + "%")
